ca3fb35fbbd107fc257cd3983997047abf80f4c3
oktawn <belyhmitia@gmail.com>
2024-04-09 19:32:57 +0500

    change page

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, request, url_for, session,redirect
+from flask import Flask, render_template, request, url_for, session, redirect
 
 app = Flask(__name__)
+app.config.from_object("config.py")
+from app import views
+
 
 @app.route("/")
 def home():
-    #our code will co here
-    return render_template("home.html")
+    user = {"nickname": "Oktawn", "policy": "student"}
+    return render_template("home.html", user=user)
 
-@app.route("/login", methods = ["POST", "GET"])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    #our code will co here
+    # our code will co here
     return render_template("login.html")
+
 
 @app.route("/logout")
 def logout():
-    #our code will co here
+    # our code will co here
     return redirect(url_for("login.html"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
