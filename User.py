9aaa329b935e488f27fd5939125aa4e379bde21b
oktawn <belyhmitia@gmail.com>
2024-04-13 18:56:13 +0500

    intermediate

--- User.py
+++ User.py
@@ -1,22 +1,21 @@
 from config import db
 
-class User():
+class User(db):
     login = db.Column(db.String, primary_key=True)
     pswd = db.Column(db.String, nullable=False)
-    fname = db.Column(db.String, default="")
-    lname = db.Column(db.String, default="")
-    group = db.Column(db.INTEGER)
-    email = db.Column(db.String, nullable=False)
     policy = db.Column(db.INTEGER, default=0)
 
-    def SignUp(login, pswd, email):
+    def Authoriz(login, pswd):
         User.login = login
         User.pswd = pswd
-        User.email = email
 
-    def SignIn(email,pswd):
-        User.email=email
-        User.pswd=pswd
+
+
+    def Logout():
+        User.login=None
+        User.pswd=None
+
 
     def __repr__(self):
         return "<User %r>" % self.login
+    
--- app.py
+++ app.py
@@ -2,30 +2,33 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from config import app, db
 from User import User
 
-user = User
-
 
 @app.route("/home", methods=["GET", "POST"])
-def home():
-    user = {"nickname": user.login, "policy": user.policy}
+def home(user):
     return render_template("home.html", user=user)
 
 
 @app.route("/")
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    user = User
     if request.method == "POST":
         login = request.form("login")
         pswd = request.form("pswd")
-        email = request.form("email")
-
-        user.SignUp(login, pswd, email)
-        try:
-            db.session.add(user)
-            db.session.commit()
-            redirect("/home")
-        except:
-            request.form("login").args.get()
+        check = user.query.filter_by(username=login).first()
+        if "butSign" in request.form:
+            if not check:
+                user.Authoriz(login, pswd)
+                try:
+                    db.session.add(user)
+                    db.session.commit()
+                    redirect("/home", user)
+                except:
+                    return render_template("login.html")
+            else:
+                render_template(
+                    "login.html", error="данный пользователь уже существует"
+                )
 
     else:
         return render_template("login.html")
@@ -33,7 +36,6 @@ def login():
 
 @app.route("/logout")
 def logout():
-    # our code will co here
     return login()
 
 
