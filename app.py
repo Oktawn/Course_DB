43a425d44bcaf9361de3694c64484e1c56773f46
oktawn <belyhmitia@gmail.com>
2024-04-17 17:07:12 +0500

    save

--- app.py
+++ app.py
@@ -1,34 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from config import app, db
-from User import User
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///DataBase.db"
+db = SQLAlchemy(app)
+
+
+class UserDB(db.Model):
+    login = db.Column(db.String, primary_key=True)
+    pswd = db.Column(db.String, nullable=False)
+    policy = db.Column(db.INTEGER, default=0)
+
+    def Authoriz(login, pswd):
+        login = login
+        pswd = pswd
+
+    def Logout():
+        login = None
+        pswd = None
+
+    def __repr__(self):
+        return "<User %r>" % self.login
 
 
 @app.route("/home", methods=["GET", "POST"])
 def home(user):
-    return render_template("home.html", user=user)
+    return render_template("home.html", user)
 
 
 @app.route("/")
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    user = User
     if request.method == "POST":
-        login = request.form("login")
-        pswd = request.form("pswd")
-        check = user.query.filter_by(username=login).first()
-        if "butSign" in request.form:
-            if not check:
-                user.Authoriz(login, pswd)
-                try:
-                    db.session.add(user)
-                    db.session.commit()
-                    redirect("/home", user)
-                except:
-                    return render_template("login.html")
-            else:
-                render_template(
-                    "login.html", error="данный пользователь уже существует"
-                )
+        login = request.form["login"]
+        pswd = request.form["pswd"]
+
+        user = UserDB(login=login, pswd=pswd, policy=0)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return redirect("/home", user)
+        except:
+            render_template("login.html")
 
     else:
         return render_template("login.html")
