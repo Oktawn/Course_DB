a8edbf74b428c72fd987001cc41d536281e9d43c
tenisaname <andreeivanovich@gmail.com>
2024-05-01 06:07:03 -0700

    added transcript

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField,PasswordField
-from check_db import create_user, check_user, get_data_user
+from wtforms import StringField, SubmitField, PasswordField, IntegerField
+from check_db import create_user, check_user, get_data_user, change_transcript
 from werkzeug.security import generate_password_hash
 
 class RestrForm(FlaskForm):
@@ -9,39 +9,53 @@ class RestrForm(FlaskForm):
     password = PasswordField("Password")
     submit = SubmitField()
 
+class TranscriptForm(FlaskForm):
+    transcript = IntegerField("Integer number")
+    submit = SubmitField()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "the random string"
 
-
+# Главная страница ученика
+@app.route("/home/<username>", methods=["GET", "POST"])
 @app.route("/home", methods=["GET", "POST"])
 def home(username):
     student = get_data_user(username)
-    group_id = str(student[-6:-2])
-    return render_template("new_home.html", user=username, group = group_id)
+    group_id = student[-1]
+    transcript = student[-3]
+    name_error = 'нету номера зачетки'
+    print(transcript)
+    print(student)
+    return render_template("new_home.html", user=username, group = group_id, transcript = transcript, name_error = name_error)
 
+# Оценки 
+@app.route('/transcript')
+def transcript():
+    return render_template("evalution.html")
 
+# Расписание
 @app.route('/schedule/<group>', methods=["GET", "POST"])
 def schedule(group):
     iframe = "https://old.tyuiu.ru/shedule_new/bin/groups.py?act=show&print=&sgroup="  + group
     return render_template("schedule.html", iframe=iframe)
 
-# @app.route("/singup", methods=["GET", "POST"])
-# def singup():
-#     form = SingUpForm()
-#     if form.validate_on_submit():
-#         username = form.username.data
-#         password = form.password.data
-#         hash_password = generate_password_hash(str(password))
-#         create_user(username, hash_password)
-#         return login()
-#     return render_template("singup.html", form=form)
+# Внести номер зачетки
+@app.route('/evaluations_input/<username>', methods=["GET", "POST"])
+def evaluations_input(username):
+    form_transcript = TranscriptForm()
+    if form_transcript.validate_on_submit():
+        transcript_num = form_transcript.transcript.data 
+        change_transcript(username, transcript_num)
+    return render_template('evaluations_input.html', form = form_transcript, user = username)
 
-@app.route("/error")
+
+# Ошибка
+@app.route("/error", methods=["GET", "POST"])
+@app.route("/error/<name_error>", methods=["GET", "POST"])
 def error(name_error):
     return render_template("error.html", name_error = name_error)
 
-
+# Основная страница
 @app.route("/", methods=["GET", "POST"])
 @app.route("/login", methods=["GET", "POST"])
 def login():
--- check_db.py
+++ check_db.py
@@ -67,25 +67,54 @@ def get_data_user(user_login):
             database= 'u2559429_efim',
             cursorclass= pymysql.cursors.DictCursor
         )
-    
+
         try:
             # Insert data into table
             with connection.cursor() as cursor:
-                insert_query = f"SELECT * FROM `student` WHERE login = '{user_login}';"
+                insert_query = f"SELECT * FROM student WHERE login = '{user_login}';"
                 cursor.execute(insert_query)
                 connection.commit()
                 result = cursor.fetchall()
-                result = str(result)
-                return result
-                
+                data = str(result)
+                for i in ['[',']','{','}']:
+                    data = data.replace(i,'')
+                    data = data.replace(' ','')
+                data = data.replace("'",'')
+                data = data.replace(",",' ')
+                data = data.replace(":",' ')
+                data = data.split(' ')
+                return data
+
         finally:
             connection.close()
 
-        
+
     except Exception as ex:
         print('Connection error')
         print(ex)
 
-data = get_data_user('efim')
-data = data[-6:-2]
-print(data)
+def change_transcript(user_login, transcript):
+    try:
+        connection = pymysql.connect(
+            host = 'server14.hosting.reg.ru',
+            port = 3306,
+            user = 'u2559429_admin',
+            password= 'Efim0909.',
+            database= 'u2559429_efim',
+            cursorclass= pymysql.cursors.DictCursor
+        )
+
+        try:
+            # Insert data into table
+            with connection.cursor() as cursor:
+                update_query = f"UPDATE `student` SET transcript = '{transcript}' WHERE login = '{user_login}'"
+                cursor.execute(update_query)
+                connection.commit()
+        finally:
+            connection.close()
+
+
+    except Exception as ex:
+        print('Connection error')
+        print(ex)
+
