dc023e628188707ee933a7873eec20c4dc04c395
oktawn <belyhmitia@gmail.com>
2024-05-14 11:35:14 +0500

    react

--- config.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///DataBase.db"
-db = SQLAlchemy(app)
